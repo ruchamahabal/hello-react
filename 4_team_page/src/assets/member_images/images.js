26235ac12ceb8aec2fa733a582d6f36e0a6fd052
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-11-29 22:10:15 +0530

    chore: set up project and add team member images and data

---
+++ 4_team_page/src/assets/member_images/images.js
@@ -0,0 +1,12 @@
+const IMAGES = {
+    upaharika: require("../member_images/upaharika.jpeg"),
+    rucha: require("../member_images/rucha.jpeg"),
+    rishav: require("../member_images/rishav-sah.jpeg"),
+    vinay: require("../member_images/vinay-puvvadi.jpeg"),
+    prateek: require("../member_images/prateek.jpeg"),
+    shubham: require("../member_images/shubham.jpeg"),
+    shailendra: require("../member_images/shailendra.jpeg"),
+    abhishek: require("../member_images/abhishek.png"),
+};
+
+export default IMAGES;
