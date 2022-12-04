3b93d91e6a717676b1ade20ccd02b0bcffe0941e
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-12-04 19:52:06 +0530

    refactor: extract change handler func
    - add function headers
    
    - convert data filtering function to a pure function

--- 4_team_page/src/components/SearchBar.js
+++ 4_team_page/src/components/SearchBar.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 import { Team } from "../data/Team.js";
 
-const getFilteredMembers = (searchText) => {
-	/*
-		return filtered members that match name/designation with searchText
-	*/
-	return Team.filter((res) => 
+/**
+ * returns filtered members that match name/designation with searchText
+ * @param {string} searchText text used to match for filtering
+ * @param {object} data data to filter from
+ */
+const getFilteredMembers = (searchText, data) => {
+	return data.filter((res) => 
 		(
 			res.name.toLowerCase().includes(searchText.toLowerCase())
 			|| res.designation.toLowerCase().includes(searchText.toLowerCase())
@@ -18,17 +20,21 @@ const SearchBar = ({ setFilteredMembers }) => {
 	// state variable and function for getting and setting the searchText
 	const [searchText, setSearchText] = useState("");
 
+	/**
+	 * Event handler for the search form 
+	 * @param {object} e event to be handled
+	 */
+	const inputHandler = (e) => {
+		e.preventDefault();
+		// get filtered member list
+		const filteredMembers = getFilteredMembers(searchText, Team);
+		// call passed function prop to set the filteredMembers in the CardContainer
+		setFilteredMembers(filteredMembers);
+	}
+
 	return (
 		<div className="search-container">
-			<form
-				onInput = {(e) => {
-					e.preventDefault();
-					// get filtered member list
-					const filteredMembers = getFilteredMembers(searchText);
-					// call passed function prop to set the filteredMembers in the CardContainer
-					setFilteredMembers(filteredMembers);
-				}}
-			>
+			<form onInput={inputHandler}>
 				<input 
 					type="text" 
 					placeholder="Search..."
