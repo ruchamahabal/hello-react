a11bb95c2599942a8a1264005b59e49c3ccf8280
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-12-20 15:07:26 +0530

    chore: Intialize app

---
+++ 9_10_custom_hook_and_context_api/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import logo from "../assets/logo.jpeg";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<div className="logo">
+				<img src={logo} alt="logo"></img>
+				<h1 className="title link">Web Pirates</h1>
+			</div>
+
+			<div className="nav-links">
+				<Link to="/" className="link">
+					Home
+				</Link>
+
+				<Link to="/about-us" className="link">
+					About Us
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ 9_10_custom_hook_and_context_api/src/components/SearchBar.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+/**
+ * @param {string} searchText text used to match for filtering
+ * @param {object} memberData data to filter from
+ * @return {object} filtered member data that matches name/designation with searchText
+ */
+const getFilteredMembers = (searchText, memberData) => {
+	return (memberData || []).filter(
+		(res) =>
+			res.name?.toLowerCase().includes(searchText.toLowerCase()) ||
+			res.login?.toLowerCase().includes(searchText.toLowerCase()) ||
+			res.company?.toLowerCase().includes(searchText.toLowerCase())
+	);
+};
+
+const SearchBar = ({ APIData, setFilteredMembers, setNoResults }) => {
+	// state variable and function for getting and setting the searchText
+	const [searchText, setSearchText] = useState("");
+
+	/**
+	 * Event handler for the search form
+	 * @param {object} e event to be handled
+	 */
+	const inputHandler = (e) => {
+		e.preventDefault();
+
+		// get filtered member list by passing APIData as the main data to filter from
+		const filteredMembers = getFilteredMembers(searchText, APIData);
+
+		if (filteredMembers?.length) {
+			setFilteredMembers(filteredMembers);
+			setNoResults(false);
+		} else {
+			setNoResults(true);
+		}
+	};
+
+	return (
+		<div className="search-container">
+			<input
+				type="search"
+				placeholder="Search..."
+				className="search-bar"
+				onChange={(e) => {
+					setSearchText(e.target.value);
+				}}
+				onKeyUp={inputHandler}
+				value={searchText}
+				autoFocus
+			/>
+		</div>
+	);
+};
+
+export default SearchBar;
